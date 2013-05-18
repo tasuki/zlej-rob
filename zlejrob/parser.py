74f368bd65e70bf5fa8cce3faf6c31064dd0c0a5
Vit Brunner <vit.brunner@gmail.com>
2013-05-18 13:18:50 +0100

    Return tuple from parser, also string from tuple

--- zlejrob/parser.py
+++ zlejrob/parser.py
@@ -7,8 +7,19 @@ class Parser:
         Return a list of functions. A function is a list of instructions,
         each of which is a tuple of (color condition, action, instruction id)
         """
-        instructions = [list(i) for i in re.split('\|', program)]
+        split = re.split('\|', program)
+        split.pop()
+        instructions = [list(i) for i in split]
         for k,func in enumerate(instructions):
-            instructions[k] = [tuple(func[i:i+2] + [10*k+i/2])
-                               for i in range(0, len(func), 2)]
-        return instructions
+            instructions[k] = tuple(tuple(func[i:i+2] + [10*k+i/2])
+                                          for i in range(0, len(func), 2))
+        return tuple(instructions)
+
+    def string_from_instructions(self, instructions):
+        """Get program string from instruction list"""
+        funcs = []
+        for func in instructions:
+            funcs.append(''.join([instruction[0] + instruction[1]
+                                  for instruction in func]))
+
+        return ''.join([func + '|' for func in funcs])
